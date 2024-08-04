5c8fea3c41698bb7ba4a625f755a775937d5f698
Дмитро Школенко <dmitroskolenko@MacBook-Air-Dmitro.local>
2024-08-04 17:16:35 +0300

    final site

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <header className={styles.header}>
-      <nav>
+      <div className={styles.burgerMenu} onClick={toggleMenu}>
+        &#9776; {/* Символ бургер-меню */}
+      </div>
+      <nav className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ''}`}>
         <ul className={styles.navList}>
-          <li><Link to="/">Головна</Link></li>
-          <li><Link to="/products">Товари</Link></li>
-          <li><Link to="/info">Інформація</Link></li>
+          <li><Link to="/" onClick={() => setIsMenuOpen(false)}>Головна</Link></li>
+          <li><Link to="/products" onClick={() => setIsMenuOpen(false)}>Товари</Link></li>
+          <li><Link to="/info" onClick={() => setIsMenuOpen(false)}>Інформація</Link></li>
         </ul>
       </nav>
     </header>
--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom';
 const TopBar = () => {
   return (
     <div className={styles.topBar}>
-      <a href="https://t.me/yourtelegram" target="_blank" rel="noopener noreferrer" className={styles.icon}>
+      <a href="" target="_blank" rel="noopener noreferrer" className={styles.icon}>
         <img src="https://upload.wikimedia.org/wikipedia/commons/8/82/Telegram_logo.svg" alt="Telegram" />
       </a>
       <div className={styles.title}><Link to="/">RNC</Link></div>
-      <a href="https://www.instagram.com/yourinstagram" target="_blank" rel="noopener noreferrer" className={styles.icon}>
+      <a href="https://www.instagram.com/rnc.ua?igsh=MWF4Z3R2MXlxOWFvNA%3D%3D&utm_source=qr" target="_blank" rel="noopener noreferrer" className={styles.icon}>
         <img src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-social-platform-icon-png-image_6315976.png" alt="Instagram" />
       </a>
     </div>
